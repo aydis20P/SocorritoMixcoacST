0ea56f49a10706fae253bc91a7717d2b9d7bc5c4
williampalacios <mich.palacios.hdz@gmail.com>
2021-03-12 16:37:35 -0600

    plantilla principal terminada, comienzo de plantilla busqueda-cliente

--- mockups/urls.py
+++ mockups/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.principal, name="principal"),
+    path('busqueda-cliente', views.busqueda_cliente, name="busqueda-cliente"),
 ]
--- mockups/views.py
+++ mockups/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 def principal(request):
      context = {}
      return render(request, 'principal.html', context)
+
+def busqueda_cliente(request):
+
+     #Aquí se trabaja toda la lógica del negocio
+
+     context = {}
+     context['frutas'] = ["manzana", "platano", "cereza"]
+     return render(request, 'busqueda-cliente.html', context)
