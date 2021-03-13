1be9e1f794b937116b26dfd29f97affa18f95cb9
williampalacios <mich.palacios.hdz@gmail.com>
2021-03-12 19:59:29 -0600

    busuqeda-cliente, simulando query de cliente

--- mockups/views.py
+++ mockups/views.py
@@ -7,7 +7,10 @@ def principal(request):
 def busqueda_cliente(request):
 
      #Aquí se trabaja toda la lógica del negocio
+     clientes = []
+     cliente = ["Homero", "1122334455", "5544332211", "Avenida Siempre Viva, 109", "Frecuente"]
+     clientes.append(cliente)
 
      context = {}
-     context['frutas'] = ["manzana", "platano", "cereza"]
+     context['clientes'] = clientes
      return render(request, 'busqueda-cliente.html', context)
