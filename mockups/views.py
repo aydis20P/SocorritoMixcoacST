f0453b1c71f755254b74829801813e0b345a9c3b
williampalacios <mich.palacios.hdz@gmail.com>
2021-03-15 10:54:30 -0600

    logica de ejemplo para mockup principal

--- mockups/views.py
+++ mockups/views.py
@@ -1,16 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def principal(request):
-     context = {}
-     return render(request, 'principal.html', context)
+     if request.method=="POST":
+          telefono = request.POST.get('telefono')
+          #simulamos la busqueda (el QUERY)
+          if telefono == "1122334455" or telefono == "5544332211":
+               request.session['telefono'] = telefono #lo agregamos a una variable de sesión
+          else:
+               request.session['telefono'] = "" #en caso contrario vaciamos la variable de sesión
+          return redirect('busqueda-cliente')
+          
+     else:
+          context = {}
+          return render(request, 'principal.html', context)
 
 def busqueda_cliente(request):
 
      #Aquí se trabaja toda la lógica del negocio
+
+     #precondición, una lista con un cliente que tambien es una lista
      clientes = []
      cliente = ["Homero", "1122334455", "5544332211", "Avenida Siempre Viva, 109", "Frecuente"]
-     clientes.append(cliente)
-
+     if not request.session.get('telefono') == "":
+          clientes.append(cliente)
      context = {}
      context['clientes'] = clientes
+
      return render(request, 'busqueda-cliente.html', context)
