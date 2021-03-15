ebbe2cd992ba52fe3c0918caf06bbe50896bbc80
Prisciliano Jimenez <prisi2003@hotmail.com>
2021-03-14 22:29:35 -0600

    menu-orden

--- mockups/urls.py
+++ mockups/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.principal, name="principal"),
     path('busqueda-cliente', views.busqueda_cliente, name="busqueda-cliente"),
+    path('menu-orden', views.menu_orden, name = "menu-orden"),
 ]
--- mockups/views.py
+++ mockups/views.py
@@ -9,8 +9,25 @@ def busqueda_cliente(request):
      #Aquí se trabaja toda la lógica del negocio
      clientes = []
      cliente = ["Homero", "1122334455", "5544332211", "Avenida Siempre Viva, 109", "Frecuente"]
-     #clientes.append(cliente)
+     clientes.append(cliente)
 
      context = {}
      context['clientes'] = clientes
      return render(request, 'busqueda-cliente.html', context)
+
+def menu_orden(request):
+     
+     
+     p1 = ["Albondiga", 60.5]
+     p2 = ["Taco azteca", 60.5]
+     p3 = ["Coca-cola", 20]
+     p4 = ["Sopa", 30]
+     platillos = []
+     platillos.append(p1)
+     platillos.append(p2)
+     platillos.append(p3)
+     platillos.append(p4)
+
+     context = {}
+     context['platillos'] = platillos
+     return render(request, 'menu-orden.html', context)
