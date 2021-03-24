0d9cba8e321b9b7d7555fb5cae073e04b1b302c3
MasterGeekMX <paclarushtaonas@gmail.com>
2021-03-23 21:18:50 -0600

    agregado y quitado de menús completos más dinámico

--- socorritomixcoacst/admin.py
+++ socorritomixcoacst/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Usuario, Cliente
+from .models import Usuario, Cliente, Platillo
 
 # Register your models here.
 admin.site.register(Usuario)
 admin.site.register(Cliente)
+admin.site.register(Platillo)
--- socorritomixcoacst/models.py
+++ socorritomixcoacst/models.py
@@ -9,62 +9,65 @@ TIPO_MENU = (('DE', 'desayunos'),('CO','comidas'),('CENAS','cenas'))
 TIPO_USUARIO = (('AD', 'administrador'),('EM','empleado'))
 
 class Cliente(models.Model):
-    nombre = models.CharField(max_length=64, null=False, blank=False)
-    telefono = models.CharField(max_length=10, null=False, blank=False, unique=True)
-    direccion = models.CharField(max_length=128, null=False, blank=False)
-    referencias = models.CharField(max_length=128, null=True, blank=True)
-    tipo = models.CharField(choices=TIPO_CLIENTE, max_length=2, null=False, blank=False)
-    telefono_alternativo = models.CharField(max_length=10, null=True, blank=True)
-    fecha_registro = models.DateField(null=False, default=timezone.now)
+	nombre = models.CharField(max_length=64, null=False, blank=False)
+	telefono = models.CharField(max_length=10, null=False, blank=False, unique=True)
+	direccion = models.CharField(max_length=128, null=False, blank=False)
+	referencias = models.CharField(max_length=128, null=True, blank=True)
+	tipo = models.CharField(choices=TIPO_CLIENTE, max_length=2, null=False, blank=False)
+	telefono_alternativo = models.CharField(max_length=10, null=True, blank=True)
+	fecha_registro = models.DateField(null=False, default=timezone.now)
 
-    def __str__(self):
-        return "Nombre: " + self.nombre + ", Tel: " + self.telefono
+	def __str__(self):
+		return "Nombre: " + self.nombre + ", Tel: " + self.telefono
 
-    def get_absolute_url(self):
-        return reverse("cliente", args=[str(self.id)])
+	def get_absolute_url(self):
+		return reverse("cliente", args=[str(self.id)])
 
 class Orden(models.Model):
-    total = models.FloatField(max_length=5, null=False)
-    promocion = models.BooleanField(null=False)
-    total_descuento = models.FloatField(max_length=5, null=True)
-    fecha = models.DateTimeField(null=False)
-    cliente = models.ForeignKey(Cliente, on_delete=models.PROTECT)
+	total = models.FloatField(max_length=5, null=False)
+	promocion = models.BooleanField(null=False)
+	total_descuento = models.FloatField(max_length=5, null=True)
+	fecha = models.DateTimeField(null=False)
+	cliente = models.ForeignKey(Cliente, on_delete=models.PROTECT)
 
 class Platillo(models.Model):
-    nombre = models.CharField(max_length=64, null=False, blank=False, unique=True)
-    precio = models.FloatField(max_length=5, null=False)
-    descripcion = models.CharField(max_length=256, null=False, blank=False)
-    tipo = models.CharField(choices=TIPO_PLATILLO, max_length=2, null=False, blank=False)
-    es_complemento = models.BooleanField(null=False)
+	nombre = models.CharField(max_length=64, null=False, blank=False, unique=True)
+	precio = models.FloatField(max_length=5, null=False)
+	descripcion = models.CharField(max_length=256, null=False, blank=False)
+	tipo = models.CharField(choices=TIPO_PLATILLO, max_length=2, null=False, blank=False)
+	es_complemento = models.BooleanField(null=False)
+
+	def __str__(self):
+		return "Nombre: " + self.nombre
 
 
 class OrdenPlatillo(models.Model):
-    sub_total = models.FloatField(max_length=5, null=False)
-    es_completa = models.BooleanField(null=False)
-    cantidad = models.IntegerField(null=False)
-    orden = models.ForeignKey(Orden, on_delete=models.PROTECT)
-    platillo = models.ForeignKey(Platillo, on_delete=models.PROTECT)
+	sub_total = models.FloatField(max_length=5, null=False)
+	es_completa = models.BooleanField(null=False)
+	cantidad = models.IntegerField(null=False)
+	orden = models.ForeignKey(Orden, on_delete=models.PROTECT)
+	platillo = models.ForeignKey(Platillo, on_delete=models.PROTECT)
 
 class Promocion(models.Model):
-    nombre = models.CharField(max_length=64, null=False, blank=False, unique=True)
-    vigencia_dias = models.IntegerField(null=True)
-    es_por_periodo = models.BooleanField(null=False, default=False)
+	nombre = models.CharField(max_length=64, null=False, blank=False, unique=True)
+	vigencia_dias = models.IntegerField(null=True)
+	es_por_periodo = models.BooleanField(null=False, default=False)
 
 class ClientePromocion(models.Model):
-    fecha_inicio = models.DateField(null=True)
-    fecha_fin = models.DateField(null=True)
-    cliente = models.ForeignKey(Cliente, on_delete=models.PROTECT)
-    promocion = models.ForeignKey(Promocion, on_delete=models.PROTECT)
+	fecha_inicio = models.DateField(null=True)
+	fecha_fin = models.DateField(null=True)
+	cliente = models.ForeignKey(Cliente, on_delete=models.PROTECT)
+	promocion = models.ForeignKey(Promocion, on_delete=models.PROTECT)
 
 class Menu(models.Model):
-    dia = models.DateField(null=False)
-    tipo = models.CharField(choices=TIPO_MENU, max_length=2, null=False, blank=False)
+	dia = models.DateField(null=False)
+	tipo = models.CharField(choices=TIPO_MENU, max_length=2, null=False, blank=False)
 
 class PlatilloMenu(models.Model):
-    disponible = models.BooleanField(null=False)
-    platillo = models.ForeignKey(Platillo, on_delete=models.PROTECT)
-    menu = models.ForeignKey(Menu, on_delete=models.PROTECT)
+	disponible = models.BooleanField(null=False)
+	platillo = models.ForeignKey(Platillo, on_delete=models.PROTECT)
+	menu = models.ForeignKey(Menu, on_delete=models.PROTECT)
 
 class Usuario(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    rol = models.CharField(choices=TIPO_USUARIO, max_length=2, null=False, blank=False)
+	user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	rol = models.CharField(choices=TIPO_USUARIO, max_length=2, null=False, blank=False)
