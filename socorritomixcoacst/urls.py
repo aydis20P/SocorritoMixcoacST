6f8f010e81f4d21c9eb351ca2191659bda7c028c
AntonioBluebird <antoniobluebird@gmail.com>
2021-03-23 19:18:52 -0600

    Nuevos cambios: Excepción IntegrityError,Comentarios en View.py para registrar_clientes y algunas validaciones en input telefono y telefonoalternativo

--- socorritomixcoacst/urls.py
+++ socorritomixcoacst/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('cliente/<pk>/', BusquedaCliente.as_view(), name='cliente'),
     path('cliente-no-encontrado', views.cliente_no_encontrado, name = "cliente-no-encontrado"),
     path('menu-orden', views.menu_orden, name = "menu-orden"),
-    path('registrar-cliente', views.registrar_cliente, name="registrar-cliente"),
     path('resumen-pedido', views.resumen_pedido, name="resumen-pedido"),
     path('perfil-cliente', views.perfil_cliente, name="perfil-cliente"),
     path("prueba", views.prueba, name="prueba"),
