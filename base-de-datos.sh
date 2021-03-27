f406cd1fdaabf292c2682c8571de5ae4cc934294
ULemar <[com2143044206@gmail.com]>
2021-03-27 05:47:43 -0600

    Platillos agregados a la BD

--- base-de-datos.sh
+++ base-de-datos.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #hay que checar que la contraseña y el nombre de la BD sean correctos
-echo "source base-de-datos.sql" | mysql -uroot -proot -D SOCORRITO_MIXCOAC_ST
+echo "source base-de-datos.sql" | mysql -root -root -D socorro
