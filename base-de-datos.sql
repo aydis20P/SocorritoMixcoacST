1bd715a0b9c33681a39c73ab3a7a019a6bf6b783
AntonioBluebird <antoniobluebird@gmail.com>
2021-03-27 17:25:23 -0600

    Bebidas mysql

--- base-de-datos.sql
+++ base-de-datos.sql
@@ -1,1 +1,10 @@
 INSERT INTO socorritomixcoacst_platillo (nombre, precio, descripcion, tipo, es_complemento) VALUES ("Arroz", "15", "Arroz rojo con chícharos y zanahoria", "ST", "1");
+INSERT INTO socorritomixcoacst_platillo (nombre, precio, descripcion, tipo, es_complemento) VALUES ("Jugo", "15", "naranja natural", "EX", "0");
+INSERT INTO socorritomixcoacst_platillo (nombre, precio, descripcion, tipo, es_complemento) VALUES ("Café de olla", "15", "Calientito y crema ", "EX", "0");
+INSERT INTO socorritomixcoacst_platillo (nombre, precio, descripcion, tipo, es_complemento) VALUES ("Café americano", "18", "Descripción del platillo", "EX", "0");
+INSERT INTO socorritomixcoacst_platillo (nombre, precio, descripcion, tipo, es_complemento) VALUES ("Capuchino", "30", "Descripción del platillo", "EX", "0");
+INSERT INTO socorritomixcoacst_platillo (nombre, precio, descripcion, tipo, es_complemento) VALUES ("Expresso", "22", "Descripción del platillo", "EX", "0");
+INSERT INTO socorritomixcoacst_platillo (nombre, precio, descripcion, tipo, es_complemento) VALUES ("Chocolate", "30", "Descripción del platillo", "EX", "0");
+INSERT INTO socorritomixcoacst_platillo (nombre, precio, descripcion, tipo, es_complemento) VALUES ("Leche Caliente", "28", "Descripción del platillo", "EX", "0");
+INSERT INTO socorritomixcoacst_platillo (nombre, precio, descripcion, tipo, es_complemento) VALUES ("Té", "15", "Descripción del platillo", "EX", "0");
+INSERT INTO socorritomixcoacst_platillo (nombre, precio, descripcion, tipo, es_complemento) VALUES ("Agua pero nescafe", "15", "Descripción del platillo", "EX", "0");
