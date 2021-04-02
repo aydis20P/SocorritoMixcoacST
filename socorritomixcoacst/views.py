2389cb1ddcbbddd811b80b4258a4e7df38387613
AntonioBluebird <antoniobluebird@gmail.com>
2021-04-01 22:20:35 -0600

    Nueva Base de Datos

--- socorritomixcoacst/views.py
+++ socorritomixcoacst/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Usuario, Cliente, Orden, OrdenPlatillo, Platillo
+from .models import Usuario, Cliente, Orden, OrdenPlatillo, Platillo, HistorialPrecio
 from django.http import HttpResponseRedirect
 from django.views.generic import ListView, DetailView
 from django.contrib import messages
@@ -363,6 +363,7 @@ def registrar_clientes(request):
 def gestion_platillos(request):
      platillo_tipo = TIPO_PLATILLO
      platillo_mod = Platillo.objects.all()
+
      if request.method == "POST":
           prueba=request.POST.get("complemento")
           
@@ -372,7 +373,7 @@ def gestion_platillos(request):
                complemento = True
           print ("chjeckbox: "+ str(prueba))
           platillos_nuevos=Platillo(nombre=request.POST.get("nom-plat"),
-                                   precio=request.POST.get("precio"),
+                                   
                                    tipo=request.POST.get("select-tipo"),
                                    es_complemento=complemento,
                                    descripcion=request.POST.get("descripcion"))
@@ -383,6 +384,9 @@ def gestion_platillos(request):
                context['platillo'] = platillo_mod
                """Guarda los datos en BD (mysql)"""
                platillos_nuevos.save()
+               #Creamos un registro que guardará el precio usando la llave foranea que es el objeto "platillos_nuevos"
+               precio = HistorialPrecio(precio=request.POST.get("precio"), platillo=platillos_nuevos)
+               precio.save()
                return render( request, 'gestion-platillos.html', context)
           #"""Si existe una excepción de IntegrityError"""
           except IntegrityError:
