c045c5d65f594412b765ebafa3221b152282735f
MasterGeekMX <paclarushtaonas@gmail.com>
2021-04-05 18:31:51 -0500

    limpieza del views.py 1

--- socorritomixcoacst/urls.py
+++ socorritomixcoacst/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from . import views
-from .views import BusquedaCliente
-from .views import PerfilCliente
-from .views import AdminCliente
+from .views import *
 
 urlpatterns = [
     path('', views.principal, name="principal"),
     path('busqueda-cliente', views.busqueda_cliente, name="busqueda-cliente"),
-    path('cliente/<pk>/', BusquedaCliente.as_view(), name='cliente'),
+    path('cliente/<pk>/', BusquedaCliente.as_view(), name="cliente"),
     path('cliente-no-encontrado', views.cliente_no_encontrado, name = "cliente-no-encontrado"),
     path('menu-orden', views.menu_orden, name = "menu-orden"),
     path('resumen-pedido', views.resumen_pedido, name="resumen-pedido"),
