5319773e49e1710e2b92e563d4f9c3a83f94dc81
MasterGeekMX <paclarushtaonas@gmail.com>
2021-06-16 21:11:47 -0500

    archivos SQL puestos al día (y los viejos borrados)

--- desayunos.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO socorritomixcoacst_desayuno (nombre, precio) VALUES ("Paquete 1", 70);
-INSERT INTO socorritomixcoacst_desayuno (nombre, precio) VALUES ("Paquete 2", 75);
-INSERT INTO socorritomixcoacst_desayuno (nombre, precio) VALUES ("Paquete 3", 85);
-INSERT INTO socorritomixcoacst_desayuno (nombre, precio) VALUES ("Paquete 4", 90);
-INSERT INTO socorritomixcoacst_desayuno (nombre, precio) VALUES ("Paquete 5", 95);
-INSERT INTO socorritomixcoacst_desayuno (nombre, precio) VALUES ("Paquete 6", 120);
-INSERT INTO socorritomixcoacst_desayuno (nombre, precio) VALUES ("Paquete infantil", 65);
